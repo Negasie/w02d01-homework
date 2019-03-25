a57321480e3d070b937f73e72d674831fcb9825f
Nick Johnson <Blackjacknick@gmail.com>
2019-03-25 15:17:31 -0600

    data types completed

---
+++ js/app.js
@@ -0,0 +1,160 @@
+console.log('hello');
+
+
+//	1. 	Data Types
+
+
+// A light switch that can be either on or off.
+// 		Boolean.
+
+// A user's email address.
+//		String.
+
+// A spaceship with a hull, laser blasters, tractor beam, 
+//	and warp drive.
+//		Object.
+
+// A list of student names from our class.
+//		Array.
+
+// A list of student names from our class, each with a location.
+//		Object.
+
+// A list of student names from our class, each with a location 
+// 	and each with a list of favorite tv shows.
+//		Object.
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
